e999ff7c995e3ba1fbf9108674cc7f2ae19b2ba2
ahbi <ivy.loking.cheang@gmail.com>
2018-03-26 08:35:00 -0400

    Added POST request handler and building api request from POST request.

--- server/server.js
+++ server/server.js
@@ -9,12 +9,67 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const bridge = {
+  datasetID: 'test',
+  accessToken: '6baca547742c6f96a6ff71b138424f21'
+};
+const queryParams = {
+  fields: [
+    'Latitude',
+    'Longitude'
+  ],
+  limit: 50,
+  poly: [
+    -122.299250, 37.707985,
+    -122.377871, 37.853778,
+    -122.556390, 37.796635,
+    -122.507638, 37.707985
+  ]
+};
 
+// Server Setup
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Handle PoST request
+app.post('/', (request, response) => {
+  if ((!Number(request.body.offset) && request.body.offset !== '0') || Number(request.body.offset) < 0) {
+    console.log(`Request parameter offset ${request.body.offset} is not a positive number`);
+    response.send({
+      success: false,
+      error: 'Request parameter offset must be a positive number'
+    });
+    console.log('Error response sent');
+    return;
+  }
+  const offset = request.body.offset ? Number(request.body.offset) : 0;
+  console.log(`POST request received with offset ${offset}`);
+
+  // Construct path based on params
+  let path = `/api/v2/${bridge.datasetID}/listings?access_token=${bridge.accessToken}`;
+  for (const key in queryParams) {
+    let params;
+    if (Array.isArray(queryParams[key])) {
+      params = queryParams[key].join(',');
+    }
+    else {
+      params = queryParams[key];
+    }
+    path += `&${key}=${params}`;
+  }
+  path += `&offset=${offset}`;
+
+  // Make request to Bridge API
+  const options = {
+    hostname: 'rets.io',
+    path: encodeURI(path),
+    method: 'GET'
+  };
+
+  console.log(JSON.stringify(options));
+});
 
 app.listen(PORT, HOST);
 console.log(`Listening on http://${HOST}:${PORT}`);
